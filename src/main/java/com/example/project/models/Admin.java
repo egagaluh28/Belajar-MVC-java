e017b5d8ce4aa7e60c4cfbcdaaf6cff787db85e8
egagaluh28 <118706321+egagaluh28@users.noreply.github.com>
2024-01-07 14:48:39 +0700

    first commit

---
+++ src/main/java/com/example/project/models/Admin.java
@@ -0,0 +1,47 @@
+//Reza Pratama
+//J0403221116
+//TPLB2
+
+package com.example.project.models;
+
+public class Admin {
+    private String kode;
+    private String nama;
+    private Integer harga;
+
+    public Admin() {
+
+    }
+
+    public Admin(String kode, String nama, Integer harga){
+        this.kode = kode;
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setHarga(Integer harga) {
+        this.harga = harga;
+    }
+
+    public Integer getHarga() {
+        return harga;
+    }
+
+
+}
